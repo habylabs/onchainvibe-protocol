739ba57b5b57ec7b5de1f5c490cb3d6b60bcde66
michaelcjoseph <michael.joseph@hey.com>
2024-07-08 23:14:29 -0500

    Write smart contract for

---
+++ contracts/Vibe.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract Vibe is ERC20, Ownable, ERC20Permit {
+    address public constant DROPPER = 0x000000000000000000000000000000000000dEaD; // Address that will airdrop tokens to holders
+    uint256 public constant DROPPER_BASE_SUPPLY = 189_000_000 ether;
+    uint256 public constant DROPPER_BASE_FEE = 21_000_000 ether;
+    uint256 public constant LP_SUPPLY = 2_100_000_000 ether;
+
+    uint256 public dropsCount; // Number of airdrops made
+
+    constructor(address _owner)
+        ERC20("Vibe", "VIBE")
+        Ownable(_owner)
+        ERC20Permit("VIBE")
+    {
+      dropsCount = 1;
+
+      _mint(DROPPER, DROPPER_BASE_SUPPLY);
+      _mint(_owner, DROPPER_BASE_FEE + LP_SUPPLY);
+    }
+
+    function drop() public onlyOwner {
+      dropsCount += 1;
+      _mint(DROPPER, DROPPER_BASE_SUPPLY * dropsCount);
+      _mint(owner(), DROPPER_BASE_FEE * dropsCount);
+    }
+}
